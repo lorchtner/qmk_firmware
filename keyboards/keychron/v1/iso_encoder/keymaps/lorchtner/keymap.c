bbd002bfe3513b56ab6d9c05778337bdddfd96d3
Florian Fichtner <lorchtner@gmail.com>
2024-05-05 14:50:26 +0200

    Enable retro-tap for space key layer-tap

--- keyboards/keychron/v1/iso_encoder/keymaps/lorchtner/keymap.c
+++ keyboards/keychron/v1/iso_encoder/keymaps/lorchtner/keymap.c
@@ -24,17 +24,16 @@ enum layers {
     FN
 };
 
-#define KC_TASK LGUI(KC_TAB)
-#define KC_FLXP LGUI(KC_E)
+#define SPACE_LAYER LT(NUMBER_LAYER, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_iso_83(
-         KC_ESC,  KC_F1,   KC_F2,         KC_F3,   KC_F4,   KC_F5,   KC_F6                   , KC_F7,   KC_F8,   KC_F9,   KC_F10,        KC_F11,  KC_F12,  KC_DEL,           KC_MNXT,
-         KC_GRV,  KC_1,    KC_2,          KC_3,    KC_4,    KC_5,    KC_6                    , KC_7,    KC_8,    KC_9,    KC_0,          KC_MINS, KC_EQL,  KC_BSPC,          KC_PGUP,
-         KC_TAB,  KC_Q,    KC_W,          KC_E,    KC_R,    KC_T,    KC_Y                    , KC_U,    KC_I,    KC_O,    KC_P,          KC_LBRC, KC_RBRC,                   KC_PGDN,
-         KC_CAPS, KC_A,    KC_S,          KC_D,    KC_F,    KC_G,    KC_H                    , KC_J,    KC_K,    KC_L,    KC_SCLN,       KC_QUOT, KC_NUHS, KC_ENT,           KC_HOME,
-         KC_LSFT, KC_NUBS, KC_Z,          KC_X,    KC_C,    KC_V,    KC_B                    , KC_N,    KC_M,    KC_COMM, KC_DOT,        KC_SLSH,          KC_RSFT, KC_UP,
-         KC_LCTL, KC_LALT, LSFT(KC_LALT),                            LT(NUMBER_LAYER, KC_SPC),                            RSFT(KC_RGUI), MO(FN),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+         KC_ESC,  KC_F1,   KC_F2,         KC_F3,   KC_F4,   KC_F5,   KC_F6      , KC_F7,   KC_F8,   KC_F9,   KC_F10,        KC_F11,  KC_F12,  KC_DEL,           KC_MNXT,
+         KC_GRV,  KC_1,    KC_2,          KC_3,    KC_4,    KC_5,    KC_6       , KC_7,    KC_8,    KC_9,    KC_0,          KC_MINS, KC_EQL,  KC_BSPC,          KC_PGUP,
+         KC_TAB,  KC_Q,    KC_W,          KC_E,    KC_R,    KC_T,    KC_Y       , KC_U,    KC_I,    KC_O,    KC_P,          KC_LBRC, KC_RBRC,                   KC_PGDN,
+         KC_CAPS, KC_A,    KC_S,          KC_D,    KC_F,    KC_G,    KC_H       , KC_J,    KC_K,    KC_L,    KC_SCLN,       KC_QUOT, KC_NUHS, KC_ENT,           KC_HOME,
+         KC_LSFT, KC_NUBS, KC_Z,          KC_X,    KC_C,    KC_V,    KC_B       , KC_N,    KC_M,    KC_COMM, KC_DOT,        KC_SLSH,          KC_RSFT, KC_UP,
+         KC_LCTL, KC_LALT, LSFT(KC_LALT),                            SPACE_LAYER,                            RSFT(KC_RGUI), MO(FN),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
     [NUMBER_LAYER] = LAYOUT_iso_83(
          XXXXXXX, XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX     , XXXXXXX, XXXXXXX     ,          XXXXXXX,
@@ -45,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          XXXXXXX, XXXXXXX   , XXXXXXX   ,                                     KC_SPC    ,                                     XXXXXXX   , XXXXXXX     , XXXXXXX, XXXXXXX     , XXXXXXX, XXXXXXX),
 
     [FN] = LAYOUT_iso_83(
-            XXXXXXX,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  XXXXXXX,            XXXXXXX,
+            XXXXXXX,  KC_BRID,  KC_BRIU,  XXXXXXX,  XXXXXXX,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  XXXXXXX,            XXXXXXX,
             XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,            XXXXXXX,
             RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,                      XXXXXXX,
             XXXXXXX,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,            XXXXXXX,
@@ -130,3 +129,12 @@ combo_t key_combos[] = {
     [CMB_F_G] = COMBO(f_g_combo, KC_ESC),
     [CMB_H_J] = COMBO(h_j_combo, KC_ENT)
 };
+
+bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case SPACE_LAYER:
+            return true;
+        default:
+            return false;
+    }
+}
